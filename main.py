2048ff22c5cc9e8b38c68711f71d3da739b801f4
betolara1 <55064373+betolara1@users.noreply.github.com>
2024-06-17 22:29:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
 lista = [] #LISTA ONDE SERA SALVO OS DADOS DO ARQUIVO IMPORTADO
